eb9fd5cfad7c821295a2b10e50ca6b7c3b4cba97
Besim Gurbuz <mbesimgurbuz@gmail.com>
2020-09-14 16:47:59 +0300

    Add DB connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
+const logger = require('./utils/logger');
 
 const port = process.env.PORT || 5000;
 
+// DB connection
+const { DB_URL, DB_USERNAME, DB_PASSWORD } = process.env;
+const conn_url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}`;
+
+mongoose.connect(conn_url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  logger.info(`Successfully connected to ${DB_URL}`);
+}).catch((err) => {
+  logger.error(err);
+});
+
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Listening: http://localhost:${port}`);
+  logger.info(`Listening: http://localhost:${port}`);
 });
