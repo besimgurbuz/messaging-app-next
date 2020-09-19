bb6850e595aca878be1666661cd0b3a1bbd6aaba
Besim Gurbuz <mbesimgurbuz@gmail.com>
2020-09-19 13:44:04 +0300

    Fix setTestEnv.js

--- .jest/setTestEnv.js
+++ .jest/setTestEnv.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
 process.env = {
-  STEAM_PROFILE_URL: 'https://api.steam.com',
-  STEAM_KEY: 'TEST_KEY',
-  STEAM_USER_ID: 'TEST_USER_ID',
-  USERNAME: 'username',
-  PASSWORD: 'pass'
+  TOKEN_SECRET: 'SECRET'
 };
