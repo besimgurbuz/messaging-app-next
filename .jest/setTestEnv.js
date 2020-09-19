b667e8c644fe7ba33c23964712c75bbf789eadd0
Besim Gurbuz <mbesimgurbuz@gmail.com>
2020-09-19 13:42:42 +0300

    Add jest test env

---
+++ .jest/setTestEnv.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+process.env = {
+  STEAM_PROFILE_URL: 'https://api.steam.com',
+  STEAM_KEY: 'TEST_KEY',
+  STEAM_USER_ID: 'TEST_USER_ID',
+  USERNAME: 'username',
+  PASSWORD: 'pass'
+};
